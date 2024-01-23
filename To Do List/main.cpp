7fc982fc0a80cff91b9e468900b656ecdf4cb06d
solimanrafi1891 <solimanrafi1891@gmail.com>
2024-01-23 20:27:01 +0600

    Update main.cpp

--- To Do List/main.cpp
+++ To Do List/main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
-
+#include <string>
 using namespace std;
+
+// Node structure for the linked list
+struct Node
+{
+    int taskId;
+    string taskName;
+    bool completed;
+    Node* next;
+};
+
+class ToDoList
+{
+
+};
+
+
 
 int main()
 {
